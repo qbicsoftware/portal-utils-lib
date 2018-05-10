19c96eda59c1c4127d8f2dacb21bfd2c4dc812c0
Luis de la Garza <delagarza@gmail.com>
2018-05-10 15:28:56 +0200

    Added tests for QBiCPortletUI

--- src/main/java/life/qbic/portal/portlet/QBiCPortletUI.java
+++ src/main/java/life/qbic/portal/portlet/QBiCPortletUI.java
@@ -1,8 +1,10 @@
 package life.qbic.portal.portlet;
 
-import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
-
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
@@ -13,10 +15,6 @@ import com.vaadin.ui.Layout;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 /**
  * This is the class from which all other QBiC portlets derive.
  */
@@ -25,58 +23,74 @@ import org.apache.logging.log4j.Logger;
 @Widgetset("life.qbic.portlet.AppWidgetSet")
 public abstract class QBiCPortletUI extends UI {
 
-    private static final Logger LOG = LogManager.getLogger(QBiCPortletUI.class);
+  static final String INFO_LABEL_CLASS = "portlet-footer";
+  static final String INFO_LABEL_ID = "qbic-portlet-info-label";
+  static final String PORTLET_PROPERTIES_FILE_PATH = "portlet.properties";
+  static final String DEFAULT_REPO = "http://github.com/qbicsoftware";
+  static final String DEFAULT_VERSION = "0.0.1-alpha";
 
-    private final static String PORTLET_REPOSITORY_URL;
-    private final static String PORTLET_VERSION;
 
-    // load values from portlet.properties in a static block so all instances will have properly initialized values
-    static {
-        boolean cleanInit = false;
-        final Properties portletProperties = new Properties();
-        try {
-            portletProperties.load(QBiCPortletUI.class.getClassLoader().getResourceAsStream("portlet.properties"));
-            cleanInit = false;
-        } catch (IOException e) {
-            LOG.error("Error loading portlet.properties. Portlet will display bogus version/url values. " + 
-                      "Check that portlet.properties is found under the /WEB-INF/classes folder (this file " + 
-                      "is copied from src/main/resources/portlet.properties during the build).", e);
-        }
-        PORTLET_VERSION = cleanInit ? StringUtils.trimToEmpty(portletProperties.getProperty("version")) : "0.0.1-alpha";
-        PORTLET_REPOSITORY_URL = cleanInit ? StringUtils.trimToEmpty(portletProperties.getProperty("repository.url")) : "http://github.com/qbicsoftware";
-    }
-    
-    @Override
-    protected final void init(final VaadinRequest request) {
-        if (StringUtils.isBlank(PORTLET_VERSION) || StringUtils.isBlank(PORTLET_REPOSITORY_URL)) {
-            LOG.error("Missing version and/or repository url properties in portlet.properties file. " +
-                      "The file should contain the 'version' and 'repository.url' properties.");
-        }
-        LOG.info("Version " + PORTLET_VERSION);
-        final VerticalLayout layout = new VerticalLayout();        
-        layout.setMargin(true);
-        // add the portlet
-        layout.addComponent(getPortletContent(request));
+  private static final Logger LOG = LogManager.getLogger(QBiCPortletUI.class);
 
-        addPortletInfo(layout);
-        setContent(layout);
+  private final String portletVersion;
+  private final String portletRepoURL;
+
+  /**
+   * Default constructor.
+   */
+  public QBiCPortletUI() {
+    // load url/version from portlet.properties
+    LOG.info("Initializing QBiCPortletUI");
+    final Properties portletProperties = new Properties();
+    try (final InputStream propertiesFileStream =
+        QBiCPortletUI.class.getClassLoader().getResourceAsStream(PORTLET_PROPERTIES_FILE_PATH)) {
+      portletProperties.load(propertiesFileStream);
+      if (!portletProperties.containsKey("version")
+          || !portletProperties.contains("repository.url")) {
+        LOG.error("Missing version and/or repository url properties in portlet.properties file. "
+            + "The file should contain the 'version' and 'repository.url' properties. "
+            + "Using default version and/or repository url.");
+      }
+    } catch (final Exception e) {
+      LOG.error("Error loading portlet.properties. Portlet will display bogus version/url values. "
+          + "Check that portlet.properties is found under the /WEB-INF/classes folder (this file "
+          + "is copied from src/main/resources/portlet.properties during the build).", e);
     }
 
-    @Override
-    public final void setContent(Component content) {
-        super.setContent(content);
-    }
+    portletVersion =
+        StringUtils.trimToEmpty(portletProperties.getProperty("version", DEFAULT_VERSION));
+    portletRepoURL =
+        StringUtils.trimToEmpty(portletProperties.getProperty("repository.url", DEFAULT_REPO));
+  }
 
-    // adds version and repository information to the passed layout
-    private void addPortletInfo(final Layout layout) {
-        final Label portletInfoLabel = 
-        new Label("version " + PORTLET_VERSION + " (<a href=\"" + PORTLET_REPOSITORY_URL + "\">" + PORTLET_REPOSITORY_URL + "</a>)", ContentMode.HTML);
-        portletInfoLabel.addStyleName("portlet-footer");
-        layout.addComponent(portletInfoLabel);
-    }
+  @Override
+  protected final void init(final VaadinRequest request) {
+    LOG.info("Version " + portletVersion);
+    final VerticalLayout layout = new VerticalLayout();
+    layout.setMargin(true);
+    // add the portlet
+    layout.addComponent(getPortletContent(request));
 
-    /**
-     * Provide the content that will be displayed.
-     */
-    protected abstract Layout getPortletContent(final VaadinRequest request);
+    addPortletInfo(layout);
+    setContent(layout);
+  }
+
+  @Override
+  public final void setContent(final Component content) {
+    super.setContent(content);
+  }
+
+  // adds version and repository information to the passed layout
+  private void addPortletInfo(final Layout layout) {
+    final Label portletInfoLabel = new Label("version " + portletVersion + " (<a href=\""
+        + portletRepoURL + "\">" + portletRepoURL + "</a>)", ContentMode.HTML);
+    portletInfoLabel.addStyleName(INFO_LABEL_CLASS);
+    portletInfoLabel.setId(INFO_LABEL_ID);
+    layout.addComponent(portletInfoLabel);
+  }
+
+  /**
+   * Provide the content that will be displayed.
+   */
+  protected abstract Layout getPortletContent(final VaadinRequest request);
 }
--- src/test/java/life/qbic/portal/portlet/QBiCPortletUITest.java
+++ src/test/java/life/qbic/portal/portlet/QBiCPortletUITest.java
@@ -1,41 +1,203 @@
 package life.qbic.portal.portlet;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 
-@RunWith(PowerMockRunner.class)
+/**
+ * Tests for QBiCPortletUI.
+ */
 public class QBiCPortletUITest {
-	
-	@Test
-	public void testMissingVersion() {
 
-	}
+  private final static Layout TEST_CONTENT = new VerticalLayout();
 
-	@Test
-	public void testMissingRepositoryUrl() {
+  @Mock
+  private VaadinRequest mockRequest;
+  @Mock
+  private Logger mockLogger;
 
-	}
+  @Rule
+  public MockitoRule rule = MockitoJUnit.rule();
 
-	@Test
-	public void testMissingPortletProperties() {
+  @BeforeClass
+  public static void loggerSetUp() {
+    System.setProperty("log4j.defaultInitOverride", Boolean.toString(true));
+    System.setProperty("log4j.ignoreTCL", Boolean.toString(true));
+  }
 
-	}
+  @Before
+  public void setUpTest() throws IOException, URISyntaxException {
+    // inject mock logger
+    Whitebox.setInternalState(QBiCPortletUI.class, "LOG", mockLogger);
+  }
 
-	@Test
-	public void testIsActuallyUsingLog4J() {
+  @Test
+  public void testMissingVersion() throws IOException, URISyntaxException {
+    // we expect a clean init, but some warning/error should be logged
+    copyPropertiesFrom("portlet.properties_no_version");
 
-	}
+    final UI mockUI = new MockUI();
+    mockUI.doInit(mockRequest, 1, "test");
 
-	@Test
-	public void testPortletInfoIsFooter() {
+    Mockito.verify(mockLogger)
+        .error(ArgumentMatchers.contains("Missing version and/or repository url"));
+    assertVersion(mockUI, QBiCPortletUI.DEFAULT_VERSION);
+    assertRepoURL(mockUI, "TEST-VALUE-NO-VERSION");
+  }
 
-	}
+  @Test
+  public void testMissingRepositoryUrl() throws IOException, URISyntaxException {
+    // we expect a clean init, but some warning/error should be logged
+    copyPropertiesFrom("portlet.properties_no_repo");
 
-	@Test
-	public void testContentIsAdded() {
+    final UI mockUI = new MockUI();
+    mockUI.doInit(mockRequest, 1, "test");
 
-	}
+    Mockito.verify(mockLogger)
+        .error(ArgumentMatchers.contains("Missing version and/or repository url"));
+    assertVersion(mockUI, "1.2.3-TEST-NOREPO");
+    assertRepoURL(mockUI, QBiCPortletUI.DEFAULT_REPO);
+  }
+
+  @Test
+  public void testMissingPortletProperties() throws IOException, URISyntaxException {
+    // we expect a clean init, but some warning/error should be logged with an exception (for the
+    // stacktrace)
+    Files.deleteIfExists(Paths.get(getClass().getClassLoader()
+        .getResource(QBiCPortletUI.PORTLET_PROPERTIES_FILE_PATH).toURI()));
+
+    final UI mockUI = new MockUI();
+    mockUI.doInit(mockRequest, 1, "test");
+
+    Mockito.verify(mockLogger).error(ArgumentMatchers.contains("Error loading portlet.properties."),
+        ArgumentMatchers.isA(Throwable.class));
+    assertVersion(mockUI, QBiCPortletUI.DEFAULT_VERSION);
+    assertRepoURL(mockUI, QBiCPortletUI.DEFAULT_REPO);
+  }
+
+  @Test
+  public void testInfoLabelContainsProperValues() throws URISyntaxException, IOException {
+    copyPropertiesFrom("portlet.properties_good");
+
+    final UI mockUI = new MockUI();
+    mockUI.doInit(mockRequest, 1, "test");
+
+    Mockito.verify(mockLogger)
+        .error(ArgumentMatchers.contains("Missing version and/or repository url"));
+    assertVersion(mockUI, "1.2.3-TEST");
+    assertRepoURL(mockUI, "TEST-VALUE");
+  }
+
+  @Test
+  public void testPortletInfoIsFooter() throws URISyntaxException, IOException {
+    // we expect portlet information to be on a footer
+    copyPropertiesFrom("portlet.properties_good");
+
+    final UI mockUI = new MockUI();
+    mockUI.doInit(mockRequest, 1, "test");
+
+    final Label infoLabel = getInfoLabel(mockUI);
+
+    assertThat(infoLabel.getStyleName(), equalTo(QBiCPortletUI.INFO_LABEL_CLASS));
+  }
+
+  @Test
+  public void testContentIsAdded() throws URISyntaxException, IOException {
+    // we expect any content the child class adds to be present
+    copyPropertiesFrom("portlet.properties_good");
+
+    final UI mockUI = new MockUI();
+    mockUI.doInit(mockRequest, 1, "test");
+
+    for (final Component component : (Layout) mockUI.getContent()) {
+      if (component == TEST_CONTENT) {
+        return;
+      }
+    }
+    fail("Content was not added.");
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullContent() throws URISyntaxException, IOException {
+    // we expect this to fail with a loud exception
+    copyPropertiesFrom("portlet.properties_good");
+
+    final UI mockUI = new MockUI();
+    ((MockUI) mockUI).useNullContent = true;
+    mockUI.doInit(mockRequest, 1, "test");
+  }
+
+  // ========== support methods/classes ============
+  private void copyPropertiesFrom(final String propertiesFilePath)
+      throws URISyntaxException, IOException {
+    final ClassLoader classLoader = getClass().getClassLoader();
+    final Path source = Paths.get(classLoader.getResource(propertiesFilePath).toURI());
+    final Path target = Paths.get(source.getParent().toString(), File.separator,
+        QBiCPortletUI.PORTLET_PROPERTIES_FILE_PATH);
+    Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+  }
+
+  private void assertVersion(final UI ui, final String version) {
+    final Label infoLabel = getInfoLabel(ui);
+    assertThat(infoLabel.getValue(), containsString("version " + version + " (<a"));
+  }
+
+  private void assertRepoURL(final UI ui, final String repoURL) {
+    final Label infoLabel = getInfoLabel(ui);
+    assertThat(infoLabel.getValue(), containsString(">" + repoURL + "</a>"));
+  }
+
+  private Label getInfoLabel(final UI ui) {
+    return getLabel(ui, QBiCPortletUI.INFO_LABEL_ID);
+  }
+
+  private Label getLabel(final UI ui, final String id) {
+    for (final Component component : (Layout) ui.getContent()) {
+      if (id.equals(component.getId())) {
+        return (Label) component;
+      }
+    }
+    throw new RuntimeException("label with id " + id + " not found in QBiCPortletUI");
+  }
+
+  @SuppressWarnings("serial")
+  private static class MockUI extends QBiCPortletUI {
+
+    volatile boolean useNullContent = false;
+
+    @Override
+    protected Layout getPortletContent(final VaadinRequest request) {
+      if (useNullContent) {
+        return null;
+      }
+      return TEST_CONTENT;
+    }
+  }
 
 }
